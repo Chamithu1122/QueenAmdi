fe7ac06ce34536b93dbfc97e7993b08b1c907652
Chamithu1122 <87743478+Chamithu1122@users.noreply.github.com>
2021-08-11 08:59:21 +0530

    Update Base.js

--- queenamdi/Base.js
+++ queenamdi/Base.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Black Amda.
+/* Copyright (C) 2020 CHAMITHU ANUJAYE.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-QueenAmdi - Black Amda
+Chamithu - Dragon bot
 */
 
 'use strict';
